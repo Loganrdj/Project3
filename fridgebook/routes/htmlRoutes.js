bfb793b6e9b643b1e6f6f1c64ccd551834776220
Fung <koufongso@gmail.com>
2020-04-29 19:23:55 -0700

    added api

--- fridgebook/routes/htmlRoutes.js
+++ fridgebook/routes/htmlRoutes.js
@@ -4,19 +4,17 @@ const db = require("../models");
 
 
 router.get("/", (req, res) => {
-    // console.log("html routers")
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-/**
- * Retrieve user's data from database
- */
+// Retrieve user's data from database
 router.get("/profile", (req, res) => {
     if (req.isAuthenticated()) {
-        db.Foods.findAll({ where: { UserId: req.user.dataValues.id } }).then(function (result) {
-            // console.log(result);
-            res.json({user_name:req.user.name,ingredients:result});
-        })
+        db.Foods.findAll({ where: { UserId: req.user.id } })
+            .then(result => res.json({ user_name: req.user.name, ingredients: result }))
+            .catch(err => res.status(422).json(err))
+    }else{
+        res.end();
     }
 });
 
